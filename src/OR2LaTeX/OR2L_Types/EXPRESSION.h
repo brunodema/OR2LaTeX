57608967c34f96d96f52ffd5b5c588a5ad4dad54
Bruno Demantova <brunoeckdema@gmail.com>
2020-12-20 18:05:08 -0300

    removed duplicate declaration of 'COEFFICIENT' at 'EXPRESSION'

--- src/OR2LaTeX/OR2L_Types/EXPRESSION.h
+++ src/OR2LaTeX/OR2L_Types/EXPRESSION.h
@@ -4,26 +4,6 @@
 
 namespace OR2L
 {
-    class COEFFICIENT
-    {
-    public:
-        COEFFICIENT(const VARIABLE &var = VARIABLE({}, ""), const double multiplier = 1.00) : _variable(var),
-                                                                                              _multiplier(multiplier) {}
-
-        COEFFICIENT(const COEFFICIENT &) = default;
-        COEFFICIENT(COEFFICIENT &&) = default;
-        virtual COEFFICIENT &operator=(const COEFFICIENT &) = default;
-        virtual COEFFICIENT &operator=(COEFFICIENT &&) = default;
-        virtual ~COEFFICIENT() = default;
-
-        inline VARIABLE Variable() const { return _variable; }
-        inline double Multiplier() const { return _multiplier; }
-
-    private:
-        VARIABLE _variable = VARIABLE({}, "");
-        double _multiplier = 1.00;
-    };
-
     class EXPRESSION
     {
     public:
