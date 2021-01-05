4674623282922e55f3dab8da5c0fa0bf4b62ded8
Bruno Demantova <brunoeckdema@gmail.com>
2021-01-05 00:59:20 +0000

    added a script to setup the container (aka working 'postcreatecommand')

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade
+sudo apt update 
+sudo apt upgrade -y
 sudo apt install -y clang-tidy clang-format clangd
-mkdir dep && sudo wget -qO- https://github.com/google/or-tools/releases/download/v8.1/or-tools_flatzinc_ubuntu-20.04_v8.1.8487.tar.gz | sudo tar -xvz -C dep/ortools --strip-components 1
-sudo apt update && sudo apt upgrade
+mkdir dep/ortools
+sudo wget -O- https://github.com/google/or-tools/releases/download/v8.1/or-tools_flatzinc_ubuntu-20.04_v8.1.8487.tar.gz | sudo tar -xvz -C dep/ortools --strip-components 1
+sudo apt update 
+sudo apt upgrade -y
