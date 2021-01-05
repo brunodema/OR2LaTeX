3d38c1e99865f850d26f898681052b3357b4fa18
Bruno Demantova <brunoeckdema@gmail.com>
2021-01-05 03:22:49 +0000

    added 'ortools' as a CMake 'FetchContent'; Changed 'setup_container.sh' (must replace this with appropriate docker commands, since the 'Dockerfile' is used by 'devcontainer.json')

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -2,7 +2,5 @@
 sudo apt update 
 sudo apt upgrade -y
 sudo apt install -y clang-tidy clang-format clangd
-mkdir dep/ortools
-sudo wget -O- https://github.com/google/or-tools/releases/download/v8.1/or-tools_flatzinc_ubuntu-20.04_v8.1.8487.tar.gz | sudo tar -xvz -C dep/ortools --strip-components 1
 sudo apt update 
 sudo apt upgrade -y
