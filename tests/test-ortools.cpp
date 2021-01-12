c51dafbc9e9a2d2d04cfc1f6af2e851077d9bf26
Bruno Demantova <brunoeckdema@gmail.com>
2021-01-12 00:53:33 +0000

    added a new example of the 'ortools' lib

--- tests/test-ortools.cpp
+++ tests/test-ortools.cpp
@@ -7,65 +7,115 @@ using operations_research::MPSolver;
 using operations_research::MPVariable;
 using or2l::base_types::ModuleTester;
 
-std::vector<std::function<void()>> ModuleTester::tests_ = {[]() {
-  // [START solver]
-  // Create the linear solver with the GLOP backend.
-  MPSolver* solver = MPSolver::CreateSolver("GLOP");
-  // [END solver]
+std::vector<std::function<void()>> ModuleTester::tests_ = {
+    []() {
+      // [START solver]
+      // Create the linear solver with the GLOP backend.
+      MPSolver* solver = MPSolver::CreateSolver("GLOP");
+      // [END solver]
 
-  // [START variables]
-  const double infinity = operations_research::MPSolver::infinity();
-  // Create the variables x and y.
-  MPVariable* const x = solver->MakeNumVar(0.0, infinity, "x");
-  MPVariable* const y = solver->MakeNumVar(0.0, infinity, "y");
+      // [START variables]
+      const double infinity = operations_research::MPSolver::infinity();
+      // Create the variables x and y.
+      MPVariable* const x = solver->MakeNumVar(0.0, infinity, "x");
+      MPVariable* const y = solver->MakeNumVar(0.0, infinity, "y");
 
-  LOG(INFO) << "Number of variables = " << solver->NumVariables();
-  // [END variables]
+      LOG(INFO) << "Number of variables = " << solver->NumVariables();
+      // [END variables]
 
-  // [START constraints]
-  // x + 7 * y <= 17.5.
-  MPConstraint* const c0 = solver->MakeRowConstraint(-infinity, 17.5, "c0");
-  c0->SetCoefficient(x, 1);
-  c0->SetCoefficient(y, 7);
+      // [START constraints]
+      // x + 7 * y <= 17.5.
+      MPConstraint* const c0 = solver->MakeRowConstraint(-infinity, 17.5, "c0");
+      c0->SetCoefficient(x, 1);
+      c0->SetCoefficient(y, 7);
 
-  // x <= 3.5.
-  MPConstraint* const c1 = solver->MakeRowConstraint(-infinity, 3.5, "c1");
-  c1->SetCoefficient(x, 1);
-  c1->SetCoefficient(y, 0);
+      // x <= 3.5.
+      MPConstraint* const c1 = solver->MakeRowConstraint(-infinity, 3.5, "c1");
+      c1->SetCoefficient(x, 1);
+      c1->SetCoefficient(y, 0);
 
-  LOG(INFO) << "Number of constraints = " << solver->NumConstraints();
-  // [END constraints]
+      LOG(INFO) << "Number of constraints = " << solver->NumConstraints();
+      // [END constraints]
 
-  // [START objective]
-  // Maximize x + 10 * y.
-  MPObjective* const objective = solver->MutableObjective();
-  objective->SetCoefficient(x, 1);
-  objective->SetCoefficient(y, 10);
-  objective->SetMaximization();
-  // [END objective]
+      // [START objective]
+      // Maximize x + 10 * y.
+      MPObjective* const objective = solver->MutableObjective();
+      objective->SetCoefficient(x, 1);
+      objective->SetCoefficient(y, 10);
+      objective->SetMaximization();
+      // [END objective]
 
-  // [START solve]
-  const MPSolver::ResultStatus result_status = solver->Solve();
-  // Check that the problem has an optimal solution.
-  if (result_status != MPSolver::OPTIMAL) {
-    LOG(FATAL) << "The problem does not have an optimal solution!";
-  }
-  // [END solve]
+      // [START solve]
+      const MPSolver::ResultStatus result_status = solver->Solve();
+      // Check that the problem has an optimal solution.
+      if (result_status != MPSolver::OPTIMAL) {
+        LOG(FATAL) << "The problem does not have an optimal solution!";
+      }
+      // [END solve]
 
-  // [START print_solution]
-  LOG(INFO) << "Solution:" << std::endl;
-  LOG(INFO) << "Objective value = " << objective->Value();
-  LOG(INFO) << "x = " << x->solution_value();
-  LOG(INFO) << "y = " << y->solution_value();
-  // [END print_solution]
+      // [START print_solution]
+      LOG(INFO) << "Solution:" << std::endl;
+      LOG(INFO) << "Objective value = " << objective->Value();
+      LOG(INFO) << "x = " << x->solution_value();
+      LOG(INFO) << "y = " << y->solution_value();
+      // [END print_solution]
 
-  // [START advanced]
-  LOG(INFO) << "\nAdvanced usage:";
-  LOG(INFO) << "Problem solved in " << solver->wall_time() << " milliseconds";
-  LOG(INFO) << "Problem solved in " << solver->iterations() << " iterations";
-  LOG(INFO) << "Problem solved in " << solver->nodes()
-            << " branch-and-bound nodes";
-  // [END advanced]
-}};  // namespace operations_research
+      // [START advanced]
+      LOG(INFO) << "\nAdvanced usage:";
+      LOG(INFO) << "Problem solved in " << solver->wall_time()
+                << " milliseconds";
+      LOG(INFO) << "Problem solved in " << solver->iterations()
+                << " iterations";
+      // LOG(INFO) << "Problem solved in " << solver->nodes() << "
+      // branch-and-bound nodes"; [END advanced]
+    },
+    []() {
+      // Create the mip solver with the SCIP backend.
+      MPSolver* solver = MPSolver::CreateSolver("SCIP");
+
+      const double infinity = solver->infinity();
+      // x and y are integer non-negative variables.
+      MPVariable* const x = solver->MakeIntVar(0.0, infinity, "x");
+      MPVariable* const y = solver->MakeIntVar(0.0, infinity, "y");
+
+      LOG(INFO) << "Number of variables = " << solver->NumVariables();
+
+      // x + 7 * y <= 17.5.
+      MPConstraint* const c0 = solver->MakeRowConstraint(-infinity, 17.5, "c0");
+      c0->SetCoefficient(x, 1);
+      c0->SetCoefficient(y, 7);
+
+      // x <= 3.5.
+      MPConstraint* const c1 = solver->MakeRowConstraint(-infinity, 3.5, "c1");
+      c1->SetCoefficient(x, 1);
+      c1->SetCoefficient(y, 0);
+
+      LOG(INFO) << "Number of constraints = " << solver->NumConstraints();
+
+      // Maximize x + 10 * y.
+      MPObjective* const objective = solver->MutableObjective();
+      objective->SetCoefficient(x, 1);
+      objective->SetCoefficient(y, 10);
+      objective->SetMaximization();
+
+      const MPSolver::ResultStatus result_status = solver->Solve();
+      // Check that the problem has an optimal solution.
+      if (result_status != MPSolver::OPTIMAL) {
+        LOG(FATAL) << "The problem does not have an optimal solution!";
+      }
+
+      LOG(INFO) << "Solution:";
+      LOG(INFO) << "Objective value = " << objective->Value();
+      LOG(INFO) << "x = " << x->solution_value();
+      LOG(INFO) << "y = " << y->solution_value();
+
+      LOG(INFO) << "\nAdvanced usage:";
+      LOG(INFO) << "Problem solved in " << solver->wall_time()
+                << " milliseconds";
+      LOG(INFO) << "Problem solved in " << solver->iterations()
+                << " iterations";
+      LOG(INFO) << "Problem solved in " << solver->nodes()
+                << " branch-and-bound nodes";
+    }};
 
 int main() { return ModuleTester::Run(); }
