774a89d9de87d7b592e903b910d44636dbcaf464
Bruno Demantova <brunoeckdema@gmail.com>
2021-01-16 11:58:55 -0300

    readd 'Dockerfile', remove 'sudo' from commands (run root instead), comment and remove unused tests

--- tests/test-or2lbinding.cpp
+++ tests/test-or2lbinding.cpp
@@ -1,3 +1,5 @@
+
+/**
 #include "Model.h"
 #include "ModuleTester.h"
 #include "ortools/linear_solver/linear_solver.h"
@@ -11,10 +13,10 @@ using or2l::base_types::ModuleTester;
 std::vector<std::function<void()>> ModuleTester::tests_ = {
     []() {
       // model binding
-      or2l::Model model("VRP");    // creates only 'or2l' object
-      model->CreateObjects();      // throws (no model type set)
-      created model->GetObject();  // throws or 'null' (no object created)
-      created model->SetSolver(
+      or2l::Model model("VRP");  // creates only 'or2l' object
+      model->CreateObjects();    // throws (no model type set)
+      model->GetObject();        // throws or 'null' (no object created)
+      model->SetSolver(
           SolverLibrary::CBC);  // creates underlying object (and pointer)
       model->GetObject();       // gets underlying object (and pointer)
       model->DestroyObjects();  // destroys underlying object (and pointer)
@@ -22,3 +24,4 @@ std::vector<std::function<void()>> ModuleTester::tests_ = {
     []() {
       // variable binding
     }};
+**/
