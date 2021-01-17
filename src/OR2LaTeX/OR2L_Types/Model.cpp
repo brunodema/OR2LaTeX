1c7d0a9d888480acd175b25080e50fc0a800b586
Bruno Demantova <brunoeckdema@gmail.com>
2021-01-17 21:34:42 +0000

    transfer 'Model' methods from header to .cpp (finally\!)

---
+++ src/OR2LaTeX/OR2L_Types/Model.cpp
@@ -0,0 +1,89 @@
+#include "Model.h"
+
+namespace or2l {
+Model::Model(const RegexString& name,
+             const std::initializer_list<Index> indexes,
+             const std::initializer_list<Variable> variables,
+             const std::initializer_list<Constraint> constraints)
+    : name_(name) {
+  for (auto&& index : indexes) {
+    symbol_map_.insert_or_assign(index.GetName(),
+                                 std::make_unique<Index>(index));
+  }
+  for (auto&& variable : variables) {
+    symbol_map_.insert_or_assign(variable.GetName(),
+                                 std::make_unique<Variable>(variable));
+  }
+  for (auto&& constraint : constraints) {
+    symbol_map_.insert_or_assign(constraint.GetName(),
+                                 std::make_unique<Constraint>(constraint));
+  }
+}
+
+[[nodiscard]] SymbolComponent* Model::Get(const RegexString& str) const {
+  // SymbolType type = symbol_map_.at(str).get()->GetType();
+  switch (const SymbolType type = symbol_map_.at(str).get()->GetType()) {
+    case SymbolType::INDEX:
+      return static_cast<Index*>(symbol_map_.at(str).get());
+      break;
+
+    case SymbolType::VARIABLE:
+      return static_cast<Variable*>(symbol_map_.at(str).get());
+      break;
+
+    case SymbolType::VARIABLE_SET:
+      return static_cast<VariableSet*>(symbol_map_.at(str).get());
+      break;
+
+    case SymbolType::CONSTRAINT:
+      return static_cast<Constraint*>(symbol_map_.at(str).get());
+      break;
+
+    default:
+      throw Exception();
+  }
+}
+
+void Model::Remove(const RegexString& str) { symbol_map_.erase(str); }
+
+void Model::AddVariable(const Variable& var) {
+  symbol_map_.insert_or_assign(var.GetName(), std::make_unique<Variable>(var));
+}
+
+void Model::RemoveVariable(const Variable& var) {
+  symbol_map_.erase(var.GetName());
+}
+
+void Model::ExtractIndexes(const Variable& var) {
+  for (auto&& index : var.GetIndexes()) {
+    symbol_map_.insert_or_assign(index.GetName(),
+                                 std::make_unique<Index>(index));
+  }
+}
+
+void Model::AddIndex(const Index& index) {
+  symbol_map_.insert_or_assign(index.GetName(), std::make_unique<Index>(index));
+}
+
+void Model::RemoveIndex(const Index& index) {
+  symbol_map_.erase(index.GetName());
+}
+
+void Model::AddVariableSet(const VariableSet& var_set) {
+  symbol_map_.insert_or_assign(var_set.GetName(),
+                               std::make_unique<VariableSet>(var_set));
+}
+
+void Model::RemoveVariableSet(const VariableSet& var_set) {
+  symbol_map_.erase(var_set.GetName());
+}
+
+void Model::AddConstraint(const Constraint& constraint) {
+  symbol_map_.insert_or_assign(constraint.GetName(),
+                               std::make_unique<Constraint>(constraint));
+}
+
+void Model::RemoveConstraint(const Constraint& constraint) {
+  symbol_map_.erase(constraint.GetName());
+}
+}  // namespace or2l
--- src/OR2LaTeX/OR2L_Types/Model.h
+++ src/OR2LaTeX/OR2L_Types/Model.h
@@ -13,89 +13,31 @@ class Model {
  public:
   Model(const RegexString& name, const std::initializer_list<Index> indexes,
         const std::initializer_list<Variable> variables,
-        const std::initializer_list<Constraint> constraints)
-      : name_(name) {
-    for (auto&& index : indexes) {
-      symbol_map_.insert_or_assign(index.GetName(),
-                                   std::make_unique<Index>(index));
-    }
-    for (auto&& variable : variables) {
-      symbol_map_.insert_or_assign(variable.GetName(),
-                                   std::make_unique<Variable>(variable));
-    }
-    for (auto&& constraint : constraints) {
-      symbol_map_.insert_or_assign(constraint.GetName(),
-                                   std::make_unique<Constraint>(constraint));
-    }
-  }
-
+        const std::initializer_list<Constraint> constraints);
   explicit Model(const RegexString& name) : name_(name) {}
   virtual ~Model() = default;
 
-  [[nodiscard]] SymbolComponent* Get(const RegexString& str) const {
-    // SymbolType type = symbol_map_.at(str).get()->GetType();
-    switch (const SymbolType type = symbol_map_.at(str).get()->GetType()) {
-      case SymbolType::INDEX:
-        return static_cast<Index*>(symbol_map_.at(str).get());
-        break;
+  [[nodiscard]] SymbolComponent* Get(const RegexString& str) const;
 
-      case SymbolType::VARIABLE:
-        return static_cast<Variable*>(symbol_map_.at(str).get());
-        break;
+  void Remove(const RegexString& str);
 
-      case SymbolType::VARIABLE_SET:
-        return static_cast<VariableSet*>(symbol_map_.at(str).get());
-        break;
+  void AddVariable(const Variable& var);
 
-      case SymbolType::CONSTRAINT:
-        return static_cast<Constraint*>(symbol_map_.at(str).get());
-        break;
+  void RemoveVariable(const Variable& var);
 
-      default:
-        throw Exception();
-    }
-  }
+  void ExtractIndexes(const Variable& var);
 
-  void Remove(const RegexString& str) { symbol_map_.erase(str); }
+  void AddIndex(const Index& index);
 
-  void AddVariable(const Variable& var) {
-    symbol_map_.insert_or_assign(var.GetName(),
-                                 std::make_unique<Variable>(var));
-  }
+  void RemoveIndex(const Index& index);
 
-  void RemoveVariable(const Variable& var) { symbol_map_.erase(var.GetName()); }
+  void AddVariableSet(const VariableSet& var_set);
 
-  void ExtractIndexes(const Variable& var) {
-    for (auto&& index : var.GetIndexes()) {
-      symbol_map_.insert_or_assign(index.GetName(),
-                                   std::make_unique<Index>(index));
-    }
-  }
+  void RemoveVariableSet(const VariableSet& var_set);
 
-  void AddIndex(const Index& index) {
-    symbol_map_.insert_or_assign(index.GetName(),
-                                 std::make_unique<Index>(index));
-  }
+  void AddConstraint(const Constraint& constraint);
 
-  void RemoveIndex(const Index& index) { symbol_map_.erase(index.GetName()); }
-
-  void AddVariableSet(const VariableSet& var_set) {
-    symbol_map_.insert_or_assign(var_set.GetName(),
-                                 std::make_unique<VariableSet>(var_set));
-  }
-
-  void RemoveVariableSet(const VariableSet& var_set) {
-    symbol_map_.erase(var_set.GetName());
-  }
-
-  void AddConstraint(const Constraint& constraint) {
-    symbol_map_.insert_or_assign(constraint.GetName(),
-                                 std::make_unique<Constraint>(constraint));
-  }
-
-  void RemoveConstraint(const Constraint& constraint) {
-    symbol_map_.erase(constraint.GetName());
-  }
+  void RemoveConstraint(const Constraint& constraint);
 
  private:
   RegexString name_ = "";
