286eba86401109b853f9c018b6a5f8ae2fe4b644
Bruno Demantova <brunoeckdema@gmail.com>
2021-01-31 03:19:37 +0000

    test: add memory related tests

---
+++ tests/test-memory.cpp
@@ -0,0 +1,36 @@
+#include "MathExpression.h"
+#include "Model.h"
+#include "ModuleTester.h"
+#include "OperatorTypes.h"
+#include "Variable.h"
+
+using or2l::Model;
+using or2l::Variable;
+
+std::vector<std::function<void()>> or2l::base_types::ModuleTester::tests_ = {
+    []() {
+      // these tests are supposed to be run locally with valgrind...
+      // 'ModuleTester' was tested by itself on 30/01/2021 for memory leaks
+      // (nothing detected)
+      Model model1("test1");
+      { std::unique_ptr<Model> model2(new Model("CBC")); }
+      std::unique_ptr<Model> model3(new Model("CBC"));
+      model1.AddVariable(Variable("var1"));
+      model1.AddVariable(Variable("var2"));
+      model3->AddVariable(Variable("var1"));
+      model3->AddVariable(Variable("var2"));
+    },
+    []() {
+      std::unique_ptr<Model> model(new Model("test2"));
+      model->AddVariable(Variable("var1"));
+      model->AddVariable(Variable("var2"));
+      // the statement below is only for the madness of it, nothing actually
+      // interesting in it
+      model->AddConstraint(Constraint(
+          "c1", MathExpression(*dynamic_cast<Variable*>(model->Get("var1")),
+                               MathExpressionOperatorTypes::LESS_EQUAL, 1)));
+    }};
+
+int main() {
+  { or2l::base_types::ModuleTester::Run(); }
+}
