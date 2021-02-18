1d9d6918f10bec6be51d788e6e460bcb351c57ad
Bruno Demantova <brunoeckdema@gmail.com>
2021-02-18 02:10:29 +0000

    refactor: add bound check

--- src/BaseTypesLib/Bounds.h
+++ src/BaseTypesLib/Bounds.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <cstdlib>
+#include <stdexcept>
 
 namespace or2l::base_types {
 struct Bounds {
+  Bounds(std::size_t lower_bound, std::size_t upper_bound)
+      : lb(lower_bound), ub(upper_bound) {
+    if (lb >= ub) {
+      throw std::invalid_argument(
+          "The lower bound can not be bigger that then upper bound.");
+    }
+  }
+
   std::size_t lb;
   std::size_t ub;
 };
