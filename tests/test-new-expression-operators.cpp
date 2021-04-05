5df2687aaf014a5f0c85a59bcf19e556ff1a96b0
Bruno Demantova <brunoeckdema@gmail.com>
2021-04-04 22:26:09 -0300

    bring implementation concept from Compiler Explorer to here

--- tests/test-new-expression-operators.cpp
+++ tests/test-new-expression-operators.cpp
@@ -3,196 +3,406 @@
 #include "symbol/Variable.h"
 #include "gtest/gtest.h"
 
-using or2l::Constant;
-using or2l::Index;
-using or2l::IndexedSymbol;
-using or2l::InnerExpression;
-using or2l::Variable;
-using or2l::VariableType;
+#define INFO(msg)
 
-TEST(NewExpressionOperators, InnerExpressionSum1)
+// #define INFO(msg) fprintf(stderr, "info: %s:%d: %s\n", __FILE__, __LINE__,
+// __PRETTY_FUNCTION__);
+
+class Symb
 {
-    // two different variables (as IndexedSymbols) + a scalar (implicit InnerExpression<T>) should yield three different
-    // spots inside the map
-    Variable x("x");
-    Variable y("y");
-    auto expr = x + y + 7.00;
-    ASSERT_EQ(expr[x], 1.00);
-    ASSERT_EQ(expr[y], 1.00);
-    ASSERT_EQ(expr[{}], 7.00);
+  public:
+    Symb() = default;
+};
+
+class Random
+{
+  public:
+    Random() = default;
+};
+
+class Vari : public Symb
+{
+  public:
+    Vari() = default;
+};
+
+class Cons : public Symb
+{
+  public:
+    Cons() = default;
+};
+
+template <class T> struct type_traits
+{
+    using parent = void;
+};
+template <> struct type_traits<Vari>
+{
+    using parent = Symb;
+};
+template <> struct type_traits<Cons>
+{
+    using parent = Symb;
+};
+// template <> struct type_traits<Random> { using parent = Symb; };
+
+template <class T, class U = T> struct inheritance_traits
+{
+    constexpr static bool has_same_parent();
+};
+template <class T, class U> constexpr bool inheritance_traits<T, U>::has_same_parent()
+{
+    using parent_type = typename type_traits<T>::parent;
+    return std::is_base_of<parent_type, T>::value && std::is_base_of<parent_type, U>::value &&
+           !std::is_same<parent_type, T>::value && !std::is_same<parent_type, U>::value;
 }
 
-TEST(NewExpressionOperators, InnerExpressionSum2)
+template <class T> class IE
 {
-    // two equal IndexedSymbols should yield one unique entry in the map
-    Variable a("a");
-    Variable b("a");
-    auto expr = a + b;
-    ASSERT_EQ(expr[a], 2.00);
-    ASSERT_EQ(expr[b], 2.00);
+  public:
+    IE() = default;
+    // solves seventh batch
+    // IE(const T &obj);
+
+    // solves fifth batch
+    template <class numeric_type, typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
+    IE<T> operator+(const numeric_type &lhs)
+    {
+        INFO("");
+        return IE<T>();
+    }
+    IE<T> operator+(const T &lhs)
+    {
+        INFO("");
+        return IE<T>();
+    }
+    // solves sixth batch
+    template <class parent_type = typename type_traits<T>::parent,
+              typename = typename std::enable_if<std::is_base_of<parent_type, T>::value>::type>
+    IE<parent_type> operator+(const parent_type &lhs)
+    {
+        INFO("");
+        return IE<parent_type>();
+    }
+    template <class parent_type = typename type_traits<T>::parent, class Child,
+              typename = typename std::enable_if<inheritance_traits<T, Child>::has_same_parent()>::type>
+    IE<parent_type> operator+(const Child &lhs)
+    {
+        INFO("");
+        return IE<parent_type>();
+    }
+    // solves eighth batch
+    template <class Child, typename = typename std::enable_if<std::is_base_of<T, Child>::value>::type>
+    IE<T> operator+(const IE<Child> &lhs)
+    {
+        INFO("");
+        return IE<T>();
+    }
+    template <class parent_type, typename = typename std::enable_if<std::is_base_of<parent_type, T>::value>::type>
+    IE<parent_type> operator+(const IE<parent_type> &lhs)
+    {
+        INFO("");
+        return IE<parent_type>();
+    }
+    template <class Child, class parent_type = typename type_traits<T>::parent,
+              typename = typename std::enable_if<inheritance_traits<T, Child>::has_same_parent()>::type>
+    IE<parent_type> operator+(const IE<Child> &lhs)
+    {
+        INFO("");
+        return IE<parent_type>();
+    }
+    // solves ninth batch
+    IE<T> operator+(const IE<T> &lhs)
+    {
+        INFO("");
+        return IE<T>();
+    }
+};
+
+// solves the first batch
+template <class T, class numeric_type,
+          typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
+IE<T> operator+(const T &lhs, const numeric_type &rhs)
+{
+    INFO("");
+    return IE<T>();
+}
+template <class T, class numeric_type,
+          typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
+IE<T> operator+(const numeric_type &lhs, const T &rhs)
+{
+    INFO("");
+    return IE<T>();
+}
+// solves the second batch
+template <class T> IE<T> operator+(const T &lhs, const T &rhs)
+{
+    INFO("");
+    return IE<T>();
+}
+template <class Child, class parent_type = typename type_traits<Child>::parent,
+          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
+IE<parent_type> operator+(const Child &lhs, const parent_type &rhs)
+{
+    INFO("");
+    return IE<parent_type>();
+}
+template <class Child, class parent_type = typename type_traits<Child>::parent,
+          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
+IE<parent_type> operator+(const parent_type &lhs, const Child &rhs)
+{
+    INFO("");
+    return IE<parent_type>();
+}
+template <class Child1, class Child2, class parent_type = typename type_traits<Child1>::parent,
+          typename = typename std::enable_if<inheritance_traits<Child1, Child2>::has_same_parent()>::type>
+IE<parent_type> operator+(const Child1 &lhs, const Child2 &rhs)
+{
+    INFO("");
+    return IE<parent_type>();
 }
 
-TEST(NewExpressionOperators, InnerExpressionSum3)
+// solves tenth batch
+template <class T, class numeric_type,
+          typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
+IE<T> operator+(const numeric_type &lhs, const IE<T> &rhs)
 {
-    // three variables, two of them differing by their type, should yield two spots on the map, since as IndexSymbols
-    // they are the same (compares name and indexes)
-    Variable a("a");
-    Variable b("b", or2l::VariableType::CONTINUOUS);
-    Variable c("b", or2l::VariableType::BINARY);
-    InnerExpression<IndexedSymbol> expr = a + b + c;
-    ASSERT_EQ(expr[a], 1.00);
-    ASSERT_EQ(expr[b], 2.00); // as a IndexedSymbol, both are the same
-    ASSERT_EQ(expr[c], 2.00); // as a IndexedSymbol, both are the same
-
-    InnerExpression<Variable> expr2 = a + b + c;
-    ASSERT_EQ(expr2[a], 1.00);
-    ASSERT_EQ(expr2[b], 1.00); // as a Variable, they are different
-    ASSERT_EQ(expr2[c], 1.00); // as a Variable, they are different
+    INFO("");
+    return IE<T>();
 }
 
-TEST(NewExpressionOperators, InnerExpressionSum4)
+// solves eleventh batch
+template <class T> IE<T> operator+(const T &lhs, const IE<T> &rhs)
 {
-    // considering that an IndexedSymbol is the parent of Variable and Constant, an sum between the two should be viable
-    Variable a("a");
-    Constant b("b");
-    auto expr = a + b;
-    ASSERT_EQ(expr[a], 1.00);
-    ASSERT_EQ(expr[b], 1.00);
+    INFO("");
+    return IE<T>();
+}
+template <class Child, class parent_type = typename type_traits<Child>::parent,
+          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
+IE<parent_type> operator+(const Child &lhs, const IE<parent_type> &rhs)
+{
+    INFO("");
+    return IE<parent_type>();
+}
+template <class Child, class parent_type = typename type_traits<Child>::parent,
+          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
+IE<parent_type> operator+(const parent_type &lhs, const IE<Child> &rhs)
+{
+    INFO("");
+    return IE<parent_type>();
+}
+template <class Child1, class Child2, class parent_type = typename type_traits<Child1>::parent,
+          typename = typename std::enable_if<inheritance_traits<Child1, Child2>::has_same_parent()>::type>
+IE<parent_type> operator+(const Child1 &lhs, const IE<Child2> &rhs)
+{
+    INFO("");
+    return IE<parent_type>();
 }
 
-TEST(NewExpressionOperators, InnerExpressionSum5)
+const Random r;
+const Symb s;
+const Vari v;
+const Cons c;
+
+TEST(test, t1)
 {
-    // doing operations with random classes - string works, char converts to int (which does not work)
-    std::string aba = "aba";
-    std::string bbb = "bbb";
-    std::string cdd = "bbb";
-    InnerExpression<std::string> expr =
-        InnerExpression<std::string>(aba) + InnerExpression<std::string>(bbb) +
-        InnerExpression<std::string>(cdd); // if I cast it normally, it sums everything up first, since there is a
-                                           // default operator for std::string summation
-    ASSERT_EQ(expr[aba], 1.00);
-    ASSERT_EQ(expr[bbb], 2.00);
-    ASSERT_EQ(expr[cdd], 2.00);
-    // there are many things to work (and learn) in terms of templates and good practices
+
+    // first batch
+    auto expr1 = 1 + v;
+    auto expr2 = 1.00 + s;
+    auto expr3 = 1.00000000000000 + r;
+    auto expr4 = v + 1;
+    auto expr5 = s + 1.00;
+    auto expr6 = r + 1.00000000000000;
+}
+TEST(test, t2)
+{
+
+    // second batch
+    auto expr1 = v + v;
+    auto expr2 = v + s;
+    auto expr3 = v + c;
+    auto expr4 = c + c;
+    auto expr5 = s + v;
+    auto expr6 = c + v;
+}
+TEST(test, t3)
+{
+    // third batch (automatically passes)
+    auto expr1 = 1 + v + 1.00;
+    auto expr2 = 1.00 + s + 1.000000000;
+    auto expr3 = 1.00000000000000 + r + 1;
+    auto expr4 = v + 1 + 1.00;
+    auto expr5 = s + 1.00 + 1.000000000;
+    auto expr6 = r + 1.00000000000000 + 1;
+}
+TEST(test, t4)
+{
+    // fourth batch (automatically passes)
+    auto expr1 = v + v + 1;
+    auto expr2 = v + s + 1.00;
+    auto expr3 = v + c + 1.0000000;
+    auto expr4 = c + c + 1.0000000;
+    auto expr5 = s + v + 1.00;
+    auto expr6 = c + v + 1;
 }
 
-TEST(NewExpressionOperators, InnerExpressionSum6)
+TEST(test, t5)
 {
-    // summation of InnerExpressions
-    Index i("i", 0, 20);
-    Index j("j", 0, 6);
-    Variable x("x", VariableType::INTEGER, {i, j});
-    Variable y("y", VariableType::CONTINUOUS, {j});
-
-    auto expr = x + x + y + y + y + 14.3;
-    auto expr2 = x + x + 5.7;
-    auto expr3 = expr + expr2;
-    ASSERT_EQ(expr3[x], 4);
-    ASSERT_EQ(expr3[y], 3);
-    ASSERT_EQ(expr3[{}], 20.0);
+    // fifth batch
+    auto expr1 = v + v + 1 + v;
+    auto expr2 = v + s + 1.00 + v;
+    auto expr3 = v + c + 1.0000000 + v;
+    auto expr4 = c + c + 1.0000000 + c;
+    auto expr5 = s + v + 1.00 + s;
+    auto expr6 = c + v + 1 + c;
 }
 
-TEST(NewExpressionOperators, InnerExpressionSum7)
+TEST(test, t6)
 {
-    // summation of Indexes - this is important for the future
-    Index i("i", 0, 20);
-    Index j("j", 0, 6);
-    Index k("k", 0, 10);
-    Index l("k", 0, 10);
-
-    auto expr = i + j + k + l + 7.29358239852348753489089089493492344;
-    ASSERT_EQ(expr[i], 1.00);
-    ASSERT_EQ(expr[j], 1.00);
-    ASSERT_EQ(expr[k], 2.00);
-    ASSERT_EQ(expr[l], 2.00);
-    ASSERT_EQ(expr[{}], 7.29358239852348753489089089493492344);
+    // sixth batch
+    auto expr1 = 1 + c + s + v + 1;
+    auto expr2 = 1 + s + c + v + 1;
+    auto expr3 = 1.0000 + c + 1 + s + 1.00 + v + 1;
+    auto expr4 = 1.0000 + 1 + s + c + 1.00 + v + 1;
+    auto expr5 = v + v + c + c + s + s;
+    auto expr6 = c + c + s + s + v + v;
+    auto expr7 = s + s + c + c + v + v + 1.0000 + 1.00 + 1;
 }
 
-TEST(NewExpressionOperators, InnerExpressionSum8)
+TEST(test, t7)
 {
-    // extra tests with the base_types class... Bounds did not work since there is a is_class restriction on the
-    // definition (that does not warn the user about its existence)
-    InnerExpression<base_types::RegexString> expr =
-        InnerExpression<base_types::RegexString>{"a"} + InnerExpression<base_types::RegexString>{"b"} +
-        InnerExpression<base_types::RegexString>{"c"} + InnerExpression<base_types::RegexString>{"d"} +
-        InnerExpression<base_types::RegexString>{"d"};
-
-    ASSERT_EQ(expr["a"], 1.00);
-    ASSERT_EQ(expr["b"], 1.00);
-    ASSERT_EQ(expr["c"], 1.00);
-    ASSERT_EQ(expr["d"], 2.00);
-    ASSERT_EQ(expr[""], 0.0);
+    // seventh batch
+    IE<Random> expr1 = 1 + r + 1;
+    IE<Symb> expr2 = 1 + s + 1;
+    IE<Cons> expr3 = 1 + c + 1;
+    IE<Vari> expr4 = 1 + v + 1;
 }
 
-TEST(NewExpressionOperators, InnerExpressionSum9)
+TEST(test, t8)
 {
-    // summation of InnerExpressions with different template types (one is base of another). This is also another
-    // important test
-    Index i("i", 0, 1000);
-    Index j("j", 6, 600);
+    // eighth batch
+    IE<Random> expr1 = 1 + r + 1;
+    IE<Symb> expr2 = 1 + s + 1;
+    IE<Cons> expr3 = 1 + c + 1;
+    IE<Vari> expr4 = 1 + v + 1;
 
-    Variable x("x", VariableType::CONTINUOUS, {i, j});
-    Variable y("y", VariableType::BINARY, {i, j});
-    Constant c("c", {j});
-    const double scalar = 556.44;
+    // the first three can not compile, since the types are not compatible
+    // auto expr5 = expr1 + expr2;
+    // auto expr6 = expr1 + expr3;
+    // auto expr7 = expr1 + expr4;
 
-    auto expr_var = x + y + scalar;
-    auto expr_symbol = c + y + 443.56;
-    auto expr_mix = expr_symbol + expr_var;
-    auto expr_mix2 = expr_var + expr_symbol;
+    auto expr8 = expr2 + expr3;
+    auto expr9 = expr2 + expr4;
+    auto expr10 = expr3 + expr4;
 
-    ASSERT_EQ(expr_mix, expr_mix2);
-    ASSERT_EQ(expr_mix[x], 1.00);
-    ASSERT_EQ(expr_mix[y], 2.00);
-    ASSERT_EQ(expr_mix[c], 1.00);
-    ASSERT_EQ(expr_mix[{}], 1000.00);
+    auto expr11 = expr2 + 1 + expr3 + v;
+    auto expr12 = expr2 + 1.0000 + expr4 + c;
+    auto expr13 = expr3 + 'a' + expr4 + s;
+    auto expr14 = expr3 + expr2;
+    auto expr15 = expr4 + expr2;
 }
-//
-// TEST(NewExpressionOperators, ExpandedExpressionTests1)
-//{
-//    Index i("i", 0, 20);
-//    Index j("j", 0, 10);
-//    Variable x("x", or2l::VariableType::CONTINUOUS, {i, j});
-//    Variable y("y", or2l::VariableType::CONTINUOUS, {i});
-//
-//    ExpandedExpression<IndexedCoefficient> exp_expr1(ExpressionOperatorType::SUMMATION, x + y + 1, {i, j});
-//    ExpandedExpression<IndexedCoefficient> exp_expr2(ExpressionOperatorType::SUMMATION, y + x + 1, {i, j});
-//    ExpandedExpression<IndexedCoefficient> exp_expr3(ExpressionOperatorType::SUMMATION, x, {i, j});
-//
-//    auto test1 = exp_expr1 + exp_expr2 + exp_expr3;
-//    ASSERT_EQ(test1.GetExpandableExpressionCoeff(exp_expr1), 2.00);
-//    ASSERT_EQ(test1.GetExpandableExpressionCoeff(exp_expr3), 1.00);
-//    ASSERT_EQ(test1.GetExpandableExpressionCoeff({}), 0.00);
-//    ASSERT_EQ(test1[y], 0.00);
-//    ASSERT_EQ(test1[x], 0.00);
-//    ASSERT_EQ(test1[{}], 0.00);
-//}
-//
-// TEST(NewExpressionOperators, NewExpressionTest)
-//{
-//    Index i("i", 0, 3);
-//    Index j("j", 0, 6);
-//    Variable x("x", or2l::VariableType::CONTINUOUS, {i, j});
-//    Variable y("y", or2l::VariableType::CONTINUOUS, {i});
-//
-//    NewExpression<IndexedCoefficient> expr1(x + y);
-//    NewExpression<IndexedCoefficient> expr2(7.00 + x);
-//    NewExpression<IndexedCoefficient> expr3(y + y + y + x + 3.00);
-//    ExpandedExpression<IndexedCoefficient> expr4(ExpressionOperatorType::SUMMATION, x, {i, j});
-//    ExpandedExpression<IndexedCoefficient> expr5(ExpressionOperatorType::SUMMATION, x, {i, j});
-//    ExpandedExpression<IndexedCoefficient> expr6(ExpressionOperatorType::SUMMATION, x + y, {i, j});
-//
-//    auto test1 = expr1 + expr2 + expr3 + expr4 + expr5 + expr6;
-//
-//    ASSERT_EQ(test1[x], 3.00);
-//    ASSERT_EQ(test1[y], 4.00);
-//    ASSERT_EQ(test1[{}], 10.00);
-//    ASSERT_EQ(test1.GetExpandableExpressionCoeff(expr4), 2.00);
-//    ASSERT_EQ(test1.GetExpandableExpressionCoeff(expr5), 2.00);
-//    ASSERT_EQ(test1.GetExpandableExpressionCoeff(expr6), 1.00);
-//
-//    //auto test1 = expr1 + expr2 + expr3 + expr4 + x + y + 5.00;
-//}
+TEST(test, t9)
+{
+    // ninth batch
+    IE<Random> expr1 = r + 1;
+    IE<Symb> expr2 = 1 + s;
+    IE<Cons> expr3 = c + 1;
+    IE<Vari> expr4 = v + v;
+
+    auto expr5 = /*expr1* +*/ expr2 + expr3 + expr4;
+    auto expr6 = expr4 + expr3 + expr2;
+    auto expr7 = expr2 + expr4 + expr3;
+    auto expr8 = expr3 + expr4 + expr2;
+    auto expr9 = expr3 + expr2 + expr4;
+    auto expr10 = expr4 + expr2 + expr3;
+}
+TEST(test, t10)
+{
+    // tenth batch
+    IE<Random> expr1 = r + 1;
+    IE<Symb> expr2 = 1 + s;
+    IE<Cons> expr3 = c + 1;
+    IE<Vari> expr4 = v + v;
+
+    auto expr5 = /*expr1* +*/ expr2 + expr3 + expr4 + 1;
+    auto expr6 = 1 + expr4 + expr3 + expr2;
+    auto expr7 = expr2 + 1 + expr4 + 1 + expr3;
+    auto expr8 = 1 + expr3 + expr4 + expr2 + 1;
+    auto expr9 = 1 + expr3 + 1.000 + expr2 + 'b' + expr4 + 1;
+    auto expr10 = expr1 + expr1;
+    auto expr11 = expr2 + 1;
+    auto expr12 = 1 + expr3;
+}
+TEST(test, t11)
+{
+    // eleventh batch
+    IE<Random> expr1 = r + 1;
+    IE<Symb> expr2 = 1 + s;
+    IE<Cons> expr3 = c + 1;
+    IE<Vari> expr4 = v + v;
+
+    auto expr5 = r + expr1;
+    auto expr6 = s + expr2;
+    auto expr7 = c + expr3;
+    auto expr8 = v + expr4;
+
+    auto expr9 = expr1 + r;
+    auto expr10 = expr2 + s;
+    auto expr11 = expr3 + c;
+    auto expr12 = expr4 + v;
+}
+TEST(test, t12)
+{
+    // twelveth batch
+    IE<Random> expr1 = r + 1;
+    IE<Symb> expr2 = 1 + s;
+    IE<Cons> expr3 = c + 1;
+    IE<Vari> expr4 = v + v;
+
+    auto expr5 = r + expr1 + r;
+
+    auto expr6 = s + expr2 + v;
+    auto expr7 = v + expr2 + s;
+    auto expr8 = v + expr3 + s;
+    auto expr9 = s + expr3 + v;
+    auto expr10 = v + expr4 + s;
+    auto expr11 = s + expr4 + v;
+
+    auto expr16 = 1 + s + 1 + expr2 + 1 + c + v;
+    auto expr17 = c + 1 + v + expr2 + 1 + s + 1;
+    auto expr18 = 1 + s + 1 + expr3 + 1 + c + v;
+    auto expr19 = c + 1 + v + expr4 + 1 + s + 1;
+}
+TEST(test, t13)
+{
+    // thirteenth batch (automatically passes)
+    IE<Random> expr1 = r + 1;
+    IE<Symb> expr2 = 1 + s;
+    IE<Cons> expr3 = c + 1;
+    IE<Vari> expr4 = v + v;
+
+    auto expr5 = expr2 + expr2;
+    auto expr6 = expr2 + expr3;
+    auto expr7 = expr3 + expr2;
+    auto expr8 = expr2 + expr4;
+    auto expr9 = expr4 + expr2;
+}
+TEST(test, t14)
+{
+    // fourteenth batch (automatically passes)
+    IE<Random> expr1 = r + 1;
+    IE<Symb> expr2 = 1 + s;
+    IE<Cons> expr3 = c + 1;
+    IE<Vari> expr4 = v + v;
+
+    auto expr5 = 1.000 + r + 1 + expr1 + 1;
+    auto expr6 = expr1 + 1 + r + r + expr1;
+    auto expr7 = 1.00 + expr2 + expr2 + s;
+    auto expr8 = expr2 + s + expr2 + c + v;
+    auto expr9 = v + c + expr2 + s + expr2;
+    auto expr10 = expr2 + 1.00 + c + 1 + v + expr3 + s + 1 + expr4 + s + v + 'a' + c + 1.000;
+}
 
 int main(int argc, char **argv)
 {
