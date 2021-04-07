19776833d7e0daf2f5a6390bedf55fc5d357bc9c
Bruno Demantova <brunoeckdema@gmail.com>
2021-04-06 23:06:26 -0300

    isolate 'InnerExpression', update tests to correspond to the original classes

---
+++ src/OR2LaTeX/symbol/InnerExpression.h
@@ -0,0 +1,168 @@
+#include "Variable.h"
+
+
+template <class T> struct type_traits
+{
+    using parent = void;
+};
+template <> struct type_traits<or2l::Variable>
+{
+    using parent = or2l::IndexedSymbol;
+};
+template <> struct type_traits<or2l::Constant>
+{
+    using parent = or2l::IndexedSymbol;
+};
+
+template <class T, class U = T> struct inheritance_traits
+{
+    constexpr static bool has_same_parent();
+};
+template <class T, class U> constexpr bool inheritance_traits<T, U>::has_same_parent()
+{
+    using parent_type = typename type_traits<T>::parent;
+    return std::is_base_of<parent_type, T>::value && std::is_base_of<parent_type, U>::value &&
+           !std::is_same<parent_type, T>::value && !std::is_same<parent_type, U>::value;
+}
+
+template <class T> class InnerExpression
+{
+  public:
+    InnerExpression() = default;
+    // solves seventh batch
+    // InnerExpression(const T &obj);
+
+    // solves fifth batch
+    template <class numeric_type, typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
+    InnerExpression<T> operator+(const numeric_type &lhs)
+    {
+
+        return InnerExpression<T>();
+    }
+    InnerExpression<T> operator+(const T &lhs)
+    {
+
+        return InnerExpression<T>();
+    }
+    // solves sixth batch
+    template <class parent_type = typename type_traits<T>::parent,
+              typename = typename std::enable_if<std::is_base_of<parent_type, T>::value>::type>
+    InnerExpression<parent_type> operator+(const parent_type &lhs)
+    {
+
+        return InnerExpression<parent_type>();
+    }
+    template <class parent_type = typename type_traits<T>::parent, class Child,
+              typename = typename std::enable_if<inheritance_traits<T, Child>::has_same_parent()>::type>
+    InnerExpression<parent_type> operator+(const Child &lhs)
+    {
+
+        return InnerExpression<parent_type>();
+    }
+    // solves eighth batch
+    template <class Child, typename = typename std::enable_if<std::is_base_of<T, Child>::value>::type>
+    InnerExpression<T> operator+(const InnerExpression<Child> &lhs)
+    {
+
+        return InnerExpression<T>();
+    }
+    template <class parent_type, typename = typename std::enable_if<std::is_base_of<parent_type, T>::value>::type>
+    InnerExpression<parent_type> operator+(const InnerExpression<parent_type> &lhs)
+    {
+
+        return InnerExpression<parent_type>();
+    }
+    template <class Child, class parent_type = typename type_traits<T>::parent,
+              typename = typename std::enable_if<inheritance_traits<T, Child>::has_same_parent()>::type>
+    InnerExpression<parent_type> operator+(const InnerExpression<Child> &lhs)
+    {
+
+        return InnerExpression<parent_type>();
+    }
+    // solves ninth batch
+    InnerExpression<T> operator+(const InnerExpression<T> &lhs)
+    {
+
+        return InnerExpression<T>();
+    }
+};
+
+// solves the first batch
+template <class T, class numeric_type,
+          typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
+InnerExpression<T> operator+(const T &lhs, const numeric_type &rhs)
+{
+
+    return InnerExpression<T>();
+}
+template <class T, class numeric_type,
+          typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
+InnerExpression<T> operator+(const numeric_type &lhs, const T &rhs)
+{
+
+    return InnerExpression<T>();
+}
+// solves the second batch
+template <class T> InnerExpression<T> operator+(const T &lhs, const T &rhs)
+{
+
+    return InnerExpression<T>();
+}
+template <class Child, class parent_type = typename type_traits<Child>::parent,
+          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
+InnerExpression<parent_type> operator+(const Child &lhs, const parent_type &rhs)
+{
+
+    return InnerExpression<parent_type>();
+}
+template <class Child, class parent_type = typename type_traits<Child>::parent,
+          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
+InnerExpression<parent_type> operator+(const parent_type &lhs, const Child &rhs)
+{
+
+    return InnerExpression<parent_type>();
+}
+template <class Child1, class Child2, class parent_type = typename type_traits<Child1>::parent,
+          typename = typename std::enable_if<inheritance_traits<Child1, Child2>::has_same_parent()>::type>
+InnerExpression<parent_type> operator+(const Child1 &lhs, const Child2 &rhs)
+{
+
+    return InnerExpression<parent_type>();
+}
+
+// solves tenth batch
+template <class T, class numeric_type,
+          typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
+InnerExpression<T> operator+(const numeric_type &lhs, const InnerExpression<T> &rhs)
+{
+
+    return InnerExpression<T>();
+}
+
+// solves eleventh batch
+template <class T> InnerExpression<T> operator+(const T &lhs, const InnerExpression<T> &rhs)
+{
+
+    return InnerExpression<T>();
+}
+template <class Child, class parent_type = typename type_traits<Child>::parent,
+          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
+InnerExpression<parent_type> operator+(const Child &lhs, const InnerExpression<parent_type> &rhs)
+{
+
+    return InnerExpression<parent_type>();
+}
+template <class Child, class parent_type = typename type_traits<Child>::parent,
+          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
+InnerExpression<parent_type> operator+(const parent_type &lhs, const InnerExpression<Child> &rhs)
+{
+
+    return InnerExpression<parent_type>();
+}
+template <class Child1, class Child2, class parent_type = typename type_traits<Child1>::parent,
+          typename = typename std::enable_if<inheritance_traits<Child1, Child2>::has_same_parent()>::type>
+InnerExpression<parent_type> operator+(const Child1 &lhs, const InnerExpression<Child2> &rhs)
+{
+
+    return InnerExpression<parent_type>();
+}
--- tests/test-new-expression-operators.cpp
+++ tests/test-new-expression-operators.cpp
@@ -1,208 +1,17 @@
 #include "symbol/Index.h"
-#include "symbol/NewExpression.h"
+#include "symbol/InnerExpression.h"
 #include "symbol/Variable.h"
 #include "gtest/gtest.h"
 
-#define INFO(msg)
+using or2l::IndexedSymbol;
+using or2l::Index;
+using or2l::Constant;
+using or2l::Variable;
 
-// #define INFO(msg) fprintf(stderr, "info: %s:%d: %s\n", __FILE__, __LINE__,
-// __PRETTY_FUNCTION__);
-
-class Symb
-{
-  public:
-    Symb() = default;
-};
-
-class Random
-{
-  public:
-    Random() = default;
-};
-
-class Vari : public Symb
-{
-  public:
-    Vari() = default;
-};
-
-class Cons : public Symb
-{
-  public:
-    Cons() = default;
-};
-
-template <class T> struct type_traits
-{
-    using parent = void;
-};
-template <> struct type_traits<Vari>
-{
-    using parent = Symb;
-};
-template <> struct type_traits<Cons>
-{
-    using parent = Symb;
-};
-// template <> struct type_traits<Random> { using parent = Symb; };
-
-template <class T, class U = T> struct inheritance_traits
-{
-    constexpr static bool has_same_parent();
-};
-template <class T, class U> constexpr bool inheritance_traits<T, U>::has_same_parent()
-{
-    using parent_type = typename type_traits<T>::parent;
-    return std::is_base_of<parent_type, T>::value && std::is_base_of<parent_type, U>::value &&
-           !std::is_same<parent_type, T>::value && !std::is_same<parent_type, U>::value;
-}
-
-template <class T> class IE
-{
-  public:
-    IE() = default;
-    // solves seventh batch
-    // IE(const T &obj);
-
-    // solves fifth batch
-    template <class numeric_type, typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
-    IE<T> operator+(const numeric_type &lhs)
-    {
-        INFO("");
-        return IE<T>();
-    }
-    IE<T> operator+(const T &lhs)
-    {
-        INFO("");
-        return IE<T>();
-    }
-    // solves sixth batch
-    template <class parent_type = typename type_traits<T>::parent,
-              typename = typename std::enable_if<std::is_base_of<parent_type, T>::value>::type>
-    IE<parent_type> operator+(const parent_type &lhs)
-    {
-        INFO("");
-        return IE<parent_type>();
-    }
-    template <class parent_type = typename type_traits<T>::parent, class Child,
-              typename = typename std::enable_if<inheritance_traits<T, Child>::has_same_parent()>::type>
-    IE<parent_type> operator+(const Child &lhs)
-    {
-        INFO("");
-        return IE<parent_type>();
-    }
-    // solves eighth batch
-    template <class Child, typename = typename std::enable_if<std::is_base_of<T, Child>::value>::type>
-    IE<T> operator+(const IE<Child> &lhs)
-    {
-        INFO("");
-        return IE<T>();
-    }
-    template <class parent_type, typename = typename std::enable_if<std::is_base_of<parent_type, T>::value>::type>
-    IE<parent_type> operator+(const IE<parent_type> &lhs)
-    {
-        INFO("");
-        return IE<parent_type>();
-    }
-    template <class Child, class parent_type = typename type_traits<T>::parent,
-              typename = typename std::enable_if<inheritance_traits<T, Child>::has_same_parent()>::type>
-    IE<parent_type> operator+(const IE<Child> &lhs)
-    {
-        INFO("");
-        return IE<parent_type>();
-    }
-    // solves ninth batch
-    IE<T> operator+(const IE<T> &lhs)
-    {
-        INFO("");
-        return IE<T>();
-    }
-};
-
-// solves the first batch
-template <class T, class numeric_type,
-          typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
-IE<T> operator+(const T &lhs, const numeric_type &rhs)
-{
-    INFO("");
-    return IE<T>();
-}
-template <class T, class numeric_type,
-          typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
-IE<T> operator+(const numeric_type &lhs, const T &rhs)
-{
-    INFO("");
-    return IE<T>();
-}
-// solves the second batch
-template <class T> IE<T> operator+(const T &lhs, const T &rhs)
-{
-    INFO("");
-    return IE<T>();
-}
-template <class Child, class parent_type = typename type_traits<Child>::parent,
-          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
-IE<parent_type> operator+(const Child &lhs, const parent_type &rhs)
-{
-    INFO("");
-    return IE<parent_type>();
-}
-template <class Child, class parent_type = typename type_traits<Child>::parent,
-          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
-IE<parent_type> operator+(const parent_type &lhs, const Child &rhs)
-{
-    INFO("");
-    return IE<parent_type>();
-}
-template <class Child1, class Child2, class parent_type = typename type_traits<Child1>::parent,
-          typename = typename std::enable_if<inheritance_traits<Child1, Child2>::has_same_parent()>::type>
-IE<parent_type> operator+(const Child1 &lhs, const Child2 &rhs)
-{
-    INFO("");
-    return IE<parent_type>();
-}
-
-// solves tenth batch
-template <class T, class numeric_type,
-          typename = typename std::enable_if<std::is_arithmetic<numeric_type>::value>::type>
-IE<T> operator+(const numeric_type &lhs, const IE<T> &rhs)
-{
-    INFO("");
-    return IE<T>();
-}
-
-// solves eleventh batch
-template <class T> IE<T> operator+(const T &lhs, const IE<T> &rhs)
-{
-    INFO("");
-    return IE<T>();
-}
-template <class Child, class parent_type = typename type_traits<Child>::parent,
-          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
-IE<parent_type> operator+(const Child &lhs, const IE<parent_type> &rhs)
-{
-    INFO("");
-    return IE<parent_type>();
-}
-template <class Child, class parent_type = typename type_traits<Child>::parent,
-          typename = typename std::enable_if<std::is_base_of<parent_type, Child>::value>::type>
-IE<parent_type> operator+(const parent_type &lhs, const IE<Child> &rhs)
-{
-    INFO("");
-    return IE<parent_type>();
-}
-template <class Child1, class Child2, class parent_type = typename type_traits<Child1>::parent,
-          typename = typename std::enable_if<inheritance_traits<Child1, Child2>::has_same_parent()>::type>
-IE<parent_type> operator+(const Child1 &lhs, const IE<Child2> &rhs)
-{
-    INFO("");
-    return IE<parent_type>();
-}
-
-const Random r;
-const Symb s;
-const Vari v;
-const Cons c;
+const static or2l::Index r("r", 0, 20);
+const static or2l::IndexedSymbol s("s", or2l::SymbolType::UNKNOWN, {});
+const static or2l::Constant c("c");
+const static or2l::Variable v("v");
 
 TEST(test, t1)
 {
@@ -246,7 +55,6 @@ TEST(test, t4)
     auto expr5 = s + v + 1.00;
     auto expr6 = c + v + 1;
 }
-
 TEST(test, t5)
 {
     // fifth batch
@@ -257,7 +65,6 @@ TEST(test, t5)
     auto expr5 = s + v + 1.00 + s;
     auto expr6 = c + v + 1 + c;
 }
-
 TEST(test, t6)
 {
     // sixth batch
@@ -269,23 +76,21 @@ TEST(test, t6)
     auto expr6 = c + c + s + s + v + v;
     auto expr7 = s + s + c + c + v + v + 1.0000 + 1.00 + 1;
 }
-
 TEST(test, t7)
 {
     // seventh batch
-    IE<Random> expr1 = 1 + r + 1;
-    IE<Symb> expr2 = 1 + s + 1;
-    IE<Cons> expr3 = 1 + c + 1;
-    IE<Vari> expr4 = 1 + v + 1;
+    InnerExpression<Index> expr1 = 1 + r + 1;
+    InnerExpression<IndexedSymbol> expr2 = 1 + s + 1;
+    InnerExpression<Constant> expr3 = 1 + c + 1;
+    InnerExpression<Variable> expr4 = 1 + v + 1;
 }
-
 TEST(test, t8)
 {
     // eighth batch
-    IE<Random> expr1 = 1 + r + 1;
-    IE<Symb> expr2 = 1 + s + 1;
-    IE<Cons> expr3 = 1 + c + 1;
-    IE<Vari> expr4 = 1 + v + 1;
+    InnerExpression<Index> expr1 = 1 + r + 1;
+    InnerExpression<IndexedSymbol> expr2 = 1 + s + 1;
+    InnerExpression<Constant> expr3 = 1 + c + 1;
+    InnerExpression<Variable> expr4 = 1 + v + 1;
 
     // the first three can not compile, since the types are not compatible
     // auto expr5 = expr1 + expr2;
@@ -305,10 +110,10 @@ TEST(test, t8)
 TEST(test, t9)
 {
     // ninth batch
-    IE<Random> expr1 = r + 1;
-    IE<Symb> expr2 = 1 + s;
-    IE<Cons> expr3 = c + 1;
-    IE<Vari> expr4 = v + v;
+    InnerExpression<Index> expr1 = r + 1;
+    InnerExpression<IndexedSymbol> expr2 = 1 + s;
+    InnerExpression<Constant> expr3 = c + 1;
+    InnerExpression<Variable> expr4 = v + v;
 
     auto expr5 = /*expr1* +*/ expr2 + expr3 + expr4;
     auto expr6 = expr4 + expr3 + expr2;
@@ -320,10 +125,10 @@ TEST(test, t9)
 TEST(test, t10)
 {
     // tenth batch
-    IE<Random> expr1 = r + 1;
-    IE<Symb> expr2 = 1 + s;
-    IE<Cons> expr3 = c + 1;
-    IE<Vari> expr4 = v + v;
+    InnerExpression<Index> expr1 = r + 1;
+    InnerExpression<IndexedSymbol> expr2 = 1 + s;
+    InnerExpression<Constant> expr3 = c + 1;
+    InnerExpression<Variable> expr4 = v + v;
 
     auto expr5 = /*expr1* +*/ expr2 + expr3 + expr4 + 1;
     auto expr6 = 1 + expr4 + expr3 + expr2;
@@ -337,10 +142,10 @@ TEST(test, t10)
 TEST(test, t11)
 {
     // eleventh batch
-    IE<Random> expr1 = r + 1;
-    IE<Symb> expr2 = 1 + s;
-    IE<Cons> expr3 = c + 1;
-    IE<Vari> expr4 = v + v;
+    InnerExpression<Index> expr1 = r + 1;
+    InnerExpression<IndexedSymbol> expr2 = 1 + s;
+    InnerExpression<Constant> expr3 = c + 1;
+    InnerExpression<Variable> expr4 = v + v;
 
     auto expr5 = r + expr1;
     auto expr6 = s + expr2;
@@ -355,10 +160,10 @@ TEST(test, t11)
 TEST(test, t12)
 {
     // twelveth batch
-    IE<Random> expr1 = r + 1;
-    IE<Symb> expr2 = 1 + s;
-    IE<Cons> expr3 = c + 1;
-    IE<Vari> expr4 = v + v;
+    InnerExpression<Index> expr1 = r + 1;
+    InnerExpression<IndexedSymbol> expr2 = 1 + s;
+    InnerExpression<Constant> expr3 = c + 1;
+    InnerExpression<Variable> expr4 = v + v;
 
     auto expr5 = r + expr1 + r;
 
@@ -377,10 +182,10 @@ TEST(test, t12)
 TEST(test, t13)
 {
     // thirteenth batch (automatically passes)
-    IE<Random> expr1 = r + 1;
-    IE<Symb> expr2 = 1 + s;
-    IE<Cons> expr3 = c + 1;
-    IE<Vari> expr4 = v + v;
+    InnerExpression<Index> expr1 = r + 1;
+    InnerExpression<IndexedSymbol> expr2 = 1 + s;
+    InnerExpression<Constant> expr3 = c + 1;
+    InnerExpression<Variable> expr4 = v + v;
 
     auto expr5 = expr2 + expr2;
     auto expr6 = expr2 + expr3;
@@ -391,10 +196,10 @@ TEST(test, t13)
 TEST(test, t14)
 {
     // fourteenth batch (automatically passes)
-    IE<Random> expr1 = r + 1;
-    IE<Symb> expr2 = 1 + s;
-    IE<Cons> expr3 = c + 1;
-    IE<Vari> expr4 = v + v;
+    InnerExpression<Index> expr1 = r + 1;
+    InnerExpression<IndexedSymbol> expr2 = 1 + s;
+    InnerExpression<Constant> expr3 = c + 1;
+    InnerExpression<Variable> expr4 = v + v;
 
     auto expr5 = 1.000 + r + 1 + expr1 + 1;
     auto expr6 = expr1 + 1 + r + r + expr1;
